3ba1c8e5c939e3fed375266baaa5ebe118b6e652
christine.scala22@gmail.com <christine.scala22@gmail.com>
2020-07-20 21:07:50 -0400

    define assert

--- test/test.js
+++ test/test.js
@@ -1,8 +1,10 @@
 //write a mocha unit test that tests your triangle function. Be sure to require your triangle.js file (../ because it's a local module) and assert.
-var triangle = require('../index.js');
+var assert=require('assert');
+var triangle = require('../triangle.js');
 
 describe('area of triangle', function (){
     it('supposed to return area of triangle', function (){
         assert.equal(triangle(10,2));
+
     });
 });
