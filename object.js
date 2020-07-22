2b461980d2fe69b7c1d8413a3b75cabb23d3b2cc
christine.scala22@gmail.com <christine.scala22@gmail.com>
2020-07-22 10:41:31 -0400

    commas

--- object.js
+++ object.js
@@ -2,22 +2,22 @@
 //method shows price and seller, learnmore shows name genre and short description
 //call both methods
 
-var camelclub = {
-    //properties
+var camelClub = {
+   
     title: "The Camel Club",
     authorName: "David Baldacci",
     genre: "crime",
     shortDescription: "A former CIA assassin leads a group of conspiracy theorists to expose corruption in the U.S. government.",
     seller: "Barnes & Noble",
     price: "8",
-    //methods
+    
     buyNow : function (){
-        return this.price + " " + this.seller;
-    }
+        return this.price + ", " + this.seller;
+    },
     learnMore : function (){
-        return this.title + " " + this.genre + " " + this.shortDescription;
-    }
+        return this.title + ", " + this.genre + ", " + this.shortDescription;
+    },
 }
 
-console.log(camelclub.buynow());
-console.log(camelclub.learnMore());
+console.log(camelClub.buyNow());
+console.log(camelClub.learnMore());
