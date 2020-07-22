021f10518b1c0a0a6bd1cc3ed38964ebe64d6710
christine.scala22@gmail.com <christine.scala22@gmail.com>
2020-07-22 19:26:39 -0400

    cha cha cha changes

--- dom.js
+++ dom.js
@@ -1,10 +1,10 @@
-//use JS to change the styles of each of the elements in your list can be as basic as .color
+//use JS to change the styles of each of the elements in your list
 document.getElementById("one").style.color = "blue";
 document.getElementById("two").style.color = "pink";
 document.getElementById("three").style.font = "bold 20px arial";
 
 //Change the class name of your second item in your list using child - in js
-document.getElementById("two").childNodes[1].className="ahahah";
+document.getElementById("secondList").childNodes[1].className="ahahah";
 
 //Change the text value of your last item in your list using sibling - in js
-document.getElementById("last").nextSibling.textContent="Final";
+document.getElementById("last").nextElementSibling.textContent="Final";
